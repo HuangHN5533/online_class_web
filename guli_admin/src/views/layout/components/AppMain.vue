3162e74a67085adeb8e4db13dea70c55c9c6c9bf
黄海宁 <553370109@qq.com>
2020-08-03 13:12:28 +0800

    在线教育平台前台用户界面及后台管理界面

---
+++ guli_admin/src/views/layout/components/AppMain.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="app-main">
+    <transition name="fade-transform" mode="out-in">
+      <!-- or name="fade" -->
+      <!-- 给路由一个唯一值 -->
+      <router-view :key="key"/>
+      <!-- 如果不同路由指向同一组件，改变路由将不会重新渲染 -->
+      <!-- <router-view/> -->
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppMain',
+  computed: {
+    // 给路由计算一个唯一值
+    key() {
+      return this.$route.name !== undefined ? this.$route.name + +new Date() : this.$route + +new Date()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app-main {
+  /*50 = navbar  */
+  min-height: calc(100vh - 50px);
+  position: relative;
+  overflow: hidden;
+}
+</style>
